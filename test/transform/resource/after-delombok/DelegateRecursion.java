839a799012882c22b55a28a4de0bcf6fab7a17ba
Philipp Eichhorn <peichhor@web.de>
2012-07-22 09:40:05 +0200

    fixing tests

--- test/transform/resource/after-delombok/DelegateRecursion.java
+++ test/transform/resource/after-delombok/DelegateRecursion.java
@@ -1,18 +1,1 @@
-class DelegateRecursionOuterMost {
-	private final DelegateRecursionCenter center = new DelegateRecursionCenter();
-	@java.lang.SuppressWarnings("all")
-	public void innerMostMethod() {
-		this.center.innerMostMethod();
-	}
-}
-class DelegateRecursionCenter {
-	private final DelegateRecursionInnerMost inner = new DelegateRecursionInnerMost();
-	@java.lang.SuppressWarnings("all")
-	public void innerMostMethod() {
-		this.inner.innerMostMethod();
-	}
-}
-class DelegateRecursionInnerMost {
-	public void innerMostMethod() {
-	}
-}
+//ignore
